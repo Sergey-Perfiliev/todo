bf72813c2c99262dd8d988b82983f9f988e2d719
Sergey-Perfiliev <sergeyperfiliev001@gmail.com>
2022-08-12 16:53:25 +0300

    Add: LocalStorage

--- src/state/Reducer.tsx
+++ src/state/Reducer.tsx
@@ -5,33 +5,38 @@ import { ActionKind, ActionType } from './Actions';
 type StateType = Todo[]
 export type { StateType }
 
+const LS_TODO_KEY = 'todo-4c4e8594-cde4-489d-b574-d70f8ac4fcc6'
+
+const defaultItem = JSON.stringify({
+	title: 'Today I should clean my room',
+	description: `Today is ${new Date(Date.now()).toLocaleDateString()}`,
+	id: generateId(),
+	status: 'pending'
+})
+
 export const initialTodoState: StateType = [
-	{
-		title: 'Today I should clean my room',
-		description: `Today is ${new Date(Date.now()).toLocaleDateString()}`,
-		id: generateId(),
-		status: 'pending'
-	}
+	...JSON.parse(localStorage.getItem(LS_TODO_KEY) ?? `[${defaultItem}]`)
 ]
 
 export const TodoReducer = (state: StateType = initialTodoState, action: ActionType) => {
 	switch (action.type) {
-		case ActionKind.Add:
-			return [
-				...state, { ...action.todo }
-			]
+		case ActionKind.Add: {
+			const newState = [...state, { ...action.todo }]
+			localStorage.setItem(LS_TODO_KEY, JSON.stringify(newState))
+			return newState
+		}
 
-		case ActionKind.Update:
-			return [
-				...state.map(todo =>
-					todo.id === action.todo.id ? { ...action.todo } : todo)
-			]
+		case ActionKind.Update: {
+			const newState = [...state.map(todo => todo.id === action.todo.id ? { ...action.todo } : todo)]
+			localStorage.setItem(LS_TODO_KEY, JSON.stringify(newState))
+			return newState
+		}
 
-		case ActionKind.Delete:
-			return [
-				...state.filter(todo =>
-					todo.id !== action.id)
-			]
+		case ActionKind.Delete: {
+			const newState = [...state.filter(todo => todo.id !== action.id)]
+			localStorage.setItem(LS_TODO_KEY, JSON.stringify(newState))
+			return newState
+		}
 
 		default:
 			return state
